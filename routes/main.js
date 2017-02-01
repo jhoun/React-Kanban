f98eca57863c2f643b60f191ee48b35f9b642852
jhoun <jayhoun@gmail.com>
2017-01-31 19:16:59 -1000

    succesfully posted to card table

--- routes/main.js
+++ routes/main.js
@@ -5,15 +5,31 @@ const Card = db.Card;
 
 router.route('/')
   .get((req, res) => {
-    console.log('req: ', req);
-    // // Project.findAll()
-    // .then((project)  => {
-    //   res.render('hello');
-    // })
-    // .catch((e) =>{
-    //   console.error(e);
-    //   res.json(e);
-    // });
+    Card.findAll()
+    .then((project)  => {
+      console.log('project: ', project);
+      res.json('Here are all the cards');
+    })
+    .catch((e) =>{
+      console.error(e);
+      res.json(e);
+    });
   })
+  .post((req,res) => {
+    Card.create({
+      title: req.body.title,
+      priority:req.body.priority,
+      status: req.body.status,
+      createdBy: req.body.createdBy,
+      assignedTo: req.body.assignedTo
+    })
+      .then((project) =>{
+        res.json("you've created a post")
+      })
+      .catch((e) =>{
+        whichError(e.errors[0].message, req);
+       res.redirect('/portfolio/new');
+      });
+  });
 
 module.exports = router;
