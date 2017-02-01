a16c2535ee04b0103dbbaae9999a082812910aef
jhoun <jayhoun@gmail.com>
2017-02-01 12:04:11 -1000

    sever is running through webpack

--- server.js
+++ server.js
@@ -1,10 +1,54 @@
 const express = require('express');
+const PORT = 8080;
 const app = express();
 const bodyParser = require('body-parser');
 const db = require('./models');
 const Card = db.Card;
 const main = require('./routes/main');
+const path = require('path');
+const fs = require('fs');
+const webpack = require('webpack');
+const webpackMiddleware = require('webpack-dev-middleware');
+const webpackHotMiddleware = require('webpack-hot-middleware');
+const config = require('./webpack.config.js');
 
+// Check to see what dev environment we are in
+const isDeveloping = process.env.NODE_ENV !== 'production';
+const port = isDeveloping ? 3000 : process.env.PORT;
+
+if (isDeveloping) {
+  app.set('host', 'http://localhost');
+  const compiler = webpack(config);
+  const middleware = webpackMiddleware(compiler, {
+    publicPath: config.output.publicPath,
+    contentBase: 'src',
+    stats: {
+      colors: true,
+      hash: false,
+      timings: true,
+      chunks: false,
+      chunkModules: false,
+      modules: false,
+    },
+  });
+  const response = (req, res) => {
+    res.write(middleware.fileSystem.readFileSync(path.resolve(__dirname, 'dist/index.html')));
+    res.end();
+  };
+
+  app.use(middleware);
+  app.use(webpackHotMiddleware(compiler));
+  //put routes before here
+  app.get('*', response);
+} else {
+  app.use(express.static(`${__dirname}/dist`));
+  app.get('*', (req, res) => {
+    res.write(
+      fs.readFileSync(path.resolve(__dirname, 'dist/index.html'))
+    );
+    res.end();
+  });
+}
 
 app.use(bodyParser.urlencoded({
   extended:true
@@ -24,11 +68,23 @@ app.get('/', (req,res) => {
     })
 })
 
-if(!module.parent){
-    app.listen(8080, () => {
-    console.log('Server started on port 8080');
-    db.sequelize.sync();
-  });
-}
+// if(!module.parent){
+//     app.listen(PORT, () => {
+//     console.log('Server started on port 8080');
+//     db.sequelize.sync();
+//   });
+// }
+
+const onStart = (err) => {
+  if (err) {
+    throw new Error(err);
+  }
+  console.info(
+    `==> 🌎 Listening on port ${port}. ` +
+    `Open up http://localhost:${port}/ in your browser.`
+  );
+};
+
+app.listen(port, 'localhost', onStart);
 
 module.exports = app;
